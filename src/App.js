5de1bf6618afc3bd4f72559af89d5aef45aca94e
Najeeb Said <tcanjb@gmail.com>
2023-08-24 03:14:35 +0500

    added app-ads.txt

--- src/App.js
+++ src/App.js
@@ -1,34 +1,36 @@
-import { useEffect } from "react";
-import { Routes, Route } from "react-router-dom";
+import {useEffect} from "react";
+import {Routes, Route} from "react-router-dom";
 import ReactGA from "react-ga4";
 
 import Homepage from "./pages/homepage";
 import About from "./pages/about";
 import Projects from "./pages/projects";
 import Contact from "./pages/contact";
+import AppAds from "./pages/AppAds";
 import Notfound from "./pages/404";
 
-import { TRACKING_ID } from "./data/tracking";
+import {TRACKING_ID} from "./data/tracking";
 import "./app.css";
 
 function App() {
-	useEffect(() => {
-		if (TRACKING_ID !== "") {
-			ReactGA.initialize(TRACKING_ID);
-		}
-	}, []);
+  useEffect(() => {
+    if (TRACKING_ID !== "") {
+      ReactGA.initialize(TRACKING_ID);
+    }
+  }, []);
 
-	return (
-		<div className="App">
-			<Routes>
-				<Route path="/" element={<Homepage />} />
-				<Route path="/about" element={<About />} />
-				<Route path="/projects" element={<Projects />} />
-				<Route path="/contact" element={<Contact />} />
-				<Route path="*" element={<Notfound />} />
-			</Routes>
-		</div>
-	);
+  return (
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Homepage/>}/>
+          <Route path="/about" element={<About/>}/>
+          <Route path="/projects" element={<Projects/>}/>
+          <Route path="/contact" element={<Contact/>}/>
+          <Route path="/app-ads.txt" element={<AppAds/>}/>
+          <Route path="*" element={<Notfound/>}/>
+        </Routes>
+      </div>
+  );
 }
 
 export default App;
---
+++ src/pages/AppAds.jsx
@@ -0,0 +1,11 @@
+// AppAdsTxt.js
+import React from "react";
+
+const AppAdsTxt = () => {
+  const adsTxtContent = `
+  google.com, pub-2067708103851582, DIRECT, f08c47fec0942fa0
+  `;
+  return <pre>{adsTxtContent}</pre>;
+};
+
+export default AppAdsTxt;
