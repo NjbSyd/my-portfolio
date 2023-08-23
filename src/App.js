f3c76b5cefe7624df53eec5a3968cea430db533e
Najeeb Said <tcanjb@gmail.com>
2023-08-24 03:30:23 +0500

    Some Changes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
-import {useEffect} from "react";
-import {Routes, Route} from "react-router-dom";
+import { useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
 import ReactGA from "react-ga4";
 
 import Homepage from "./pages/homepage";
 import About from "./pages/about";
 import Projects from "./pages/projects";
 import Contact from "./pages/contact";
-import AppAds from "./pages/AppAds";
 import Notfound from "./pages/404";
 
-import {TRACKING_ID} from "./data/tracking";
+import { TRACKING_ID } from "./data/tracking";
 import "./app.css";
 
 function App() {
@@ -19,15 +18,20 @@ function App() {
     }
   }, []);
 
+  useEffect(() => {
+    if (window.location.pathname === "/app-ads.txt") {
+      window.location.replace("https://cdn.jsdelivr.net/gh/NjbSyd/my-portfolio@master/public/app-ads.txt");
+    }
+  }, []);
+
   return (
       <div className="App">
         <Routes>
-          <Route path="/" element={<Homepage/>}/>
-          <Route path="/about" element={<About/>}/>
-          <Route path="/projects" element={<Projects/>}/>
-          <Route path="/contact" element={<Contact/>}/>
-          <Route path="/app-ads.txt" element={<AppAds/>}/>
-          <Route path="*" element={<Notfound/>}/>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={<Notfound />} />
         </Routes>
       </div>
   );
